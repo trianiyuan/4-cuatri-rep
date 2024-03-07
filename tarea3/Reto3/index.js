b6c25b66fa5aeb079ec8de9510fc631d561ad9c1
trianiyuan <triani001@gmail.com>
2024-03-06 22:46:05 -0600

    avance tarea

---
+++ tarea3/Reto3/index.js
@@ -0,0 +1,15 @@
+document.getElementById('prevCartelera').addEventListener('click', function() {
+    $('#carouselCartelera').carousel('prev');
+});
+
+document.getElementById('nextCartelera').addEventListener('click', function() {
+    $('#carouselCartelera').carousel('next');
+});
+
+document.getElementById('prevEstrenos').addEventListener('click', function() {
+    $('#carouselCartelera').carousel('prev');
+});
+
+document.getElementById('nextEstrenos').addEventListener('click', function() {
+    $('#carouselCartelera').carousel('next');
+});
