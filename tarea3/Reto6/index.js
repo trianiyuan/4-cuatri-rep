45714936c20baf856350958035611ab8328fc578
trianiyuan <triani001@gmail.com>
2024-03-10 20:53:43 -0600

    avance tarea

---
+++ tarea3/Reto6/index.js
@@ -0,0 +1,29 @@
+function c1() {
+    document.getElementById("vehiculo").src = "imagenes/gris.png";
+    document.getElementById("color").innerHTML = "RED MICA METALLIC (3R3)";
+}
+
+function c2() {
+    document.getElementById("vehiculo").src = "imagenes/grisoscuro.png";
+    document.getElementById("color").innerHTML = "ATTITUDE GREY (218)";
+}
+
+function c3() {
+    document.getElementById("vehiculo").src = "imagenes/rojo.png";
+    document.getElementById("color").innerHTML = "SUPER RED II (040)";
+}
+
+function c4() {
+    document.getElementById("vehiculo").src = "imagenes/vino.png";
+    document.getElementById("color").innerHTML = "WINE METALLIC (4R8)";
+}
+
+function c5() {
+    document.getElementById("vehiculo").src = "imagenes/turquesa.png";
+    document.getElementById("color").innerHTML = "TURCOIS METALLIC (6W2)";
+}
+
+function c6() {
+    document.getElementById("vehiculo").src = "imagenes/negro.png";
+    document.getElementById("color").innerHTML = "BLACK METALLIC (4T1)";
+}
